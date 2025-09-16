3c7cc2a024809f74c5dd07c3ac5e4f97188cacb6
CarlosG <carlosrjg7@gmail.com>
2025-09-16 05:06:30 -0500

    final final

--- src/components/jobs/sidebar/Sidebar.tsx
+++ src/components/jobs/sidebar/Sidebar.tsx
@@ -12,16 +12,23 @@ export default function Sidebar({ jobs, fnJobSelected }: SidebarProps) {
   const [currentJobIndex, setCurrentJobIndex] = useState(0);
 
   const handlePrevious = () => {
+    if (currentJobIndex === 0) return;
     setCurrentJobIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+
+    //  -- si es pantalla mobile no se ejecuta esta función --
+    // (se coloco esta validación solo para evitar confunsion de la funcionalidad de ver detalles),
+    // en mobile debe ser un paginado. queda pendiente desarrollarla
+    if (window.innerWidth < 640) return;
     fnJobSelected(jobs[currentJobIndex - 1]);
   };
 
   const handleNext = () => {
-    // comprobar que no se pasa del último índice
     if (currentJobIndex < jobs.length - 1) {
       setCurrentJobIndex((prevIndex) =>
         Math.min(prevIndex + 1, jobs.length - 1)
       );
+
+      if (window.innerWidth < 640) return;
       fnJobSelected(jobs[currentJobIndex + 1]);
     }
   };
@@ -45,7 +52,7 @@ export default function Sidebar({ jobs, fnJobSelected }: SidebarProps) {
           ))}
         </div>
       </SimpleBar>
-      <div className='absolute bottom-0 w-2/3 md:w-full flex justify-between gap-4 px-3 bg-white py-2'>
+      <div className='absolute bottom-0 w-full flex justify-between gap-4 px-3 pb-4 bg-white py-2'>
         <button
           type='button'
           className='w-full bg-primary-200 text-font font-semibold font-poppins text-xs rounded-2xl px-4 py-2'
